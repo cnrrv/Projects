b96dd3f46ae5d07cc0ad670cc3c6747e68ae7c04
cnrrv <tessrave@gmail.com>
2023-01-16 21:54:19 +0300

    little changes navbar

--- Science/script.js
+++ Science/script.js
@@ -4,10 +4,3 @@ const myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 })
-
-$(window).resize(function(e) {
-  let width = e.target.innerWidth
-    if(width < 1200) {
-      //меняем элементы в html
-   }
-});
